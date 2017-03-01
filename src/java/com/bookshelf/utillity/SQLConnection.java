dd8da0e79bf1e8247d89764742573766a987544d
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-03-02 01:30:06 +0530

    Added Back End support

---
+++ src/java/com/bookshelf/utillity/SQLConnection.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookshelf.utillity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author aditya
+ */
+public class SQLConnection {
+    public static Connection createConnection() {
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection(  
+            "jdbc:mysql://localhost:3306/bookshelf?zeroDateTimeBehavior=convertToNull","root","");  
+             return con;
+        } catch (Exception e) {
+            System.out.println(""+e);
+        }
+        return null;        
+    }
+}
